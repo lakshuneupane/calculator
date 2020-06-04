f3c8ca49220c5d04c20fb5bef8ddc5ef0d29dd72
shyamuneupane <neupaneshyamu@gmail.com>
2020-06-04 09:12:16 -0400

    clean up

--- script.js
+++ script.js
@@ -1,34 +1,8 @@
 var firstnum ="";
 var secondnum ="";
 
-function getHistory() {
-    document.getElementsById("history-value").innerText;
-}
-function printHistory(num) {
-    document.getElementsById("history-value").innerText = num;
-}
-
-function getOutput() {
-    document.getElementsById("output-value").innerText;
-}
-
-
-
 function numberclick(num){
    
         firstnum= firstnum+num;
-        document.getElementById("historyvalue").innerText = firstnum;
-    console.log(num);
-
+        document.getElementById("Output-value").innerText = firstnum;
 }
-function getFormattedNumber(num) {
-    var n = number(num);
-    var value = n.toLocalString("en");
-    return value;
-}
-function reverseNumberFormat(num) {
-    return number(num.replace(/,/g, ''));
-}
-var operator = document.getElementsByClassName("operator");
-
-var operator = document.getElementsByClassName("number");
